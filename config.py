8e75779079cdc0e63ae896d09d81ab8f583b1ccd
rawhacker780085 <153677901+rawhacker780085@users.noreply.github.com>
2025-01-06 02:14:43 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,9 +1,9 @@
 from os import getenv
 
-API_ID = int(getenv("API_ID", "13335263"))
-API_HASH = getenv("API_HASH", "")
-BOT_TOKEN = getenv("BOT_TOKEN", "")
-OWNER_ID = int(getenv("OWNER_ID", ""))
+API_ID = int(getenv("API_ID", "28372387"))
+API_HASH = getenv("API_HASH", "11edc85521fb2d8f75e751bb284507e1")
+BOT_TOKEN = getenv("BOT_TOKEN", "8008903575:AAHIsqouHROwJoNPKyMMSjBz2blYBBfJhHo")
+OWNER_ID = int(getenv("OWNER_ID", "STINER_KING_OP"))
 STRING_SESSION = getenv("STRING_SESSION", "")
 SUDO_USERS = list(map(int, getenv("SUDO_USERS", "5392070730").split()))
 ALIVE_PIC = getenv("ALIVE_PIC", "https://telegra.ph/file/a62b9c7d9848afde0569e.jpg")
